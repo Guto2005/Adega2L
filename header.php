c46f177febb06f076e30daf833c977df0805e619
GUSTAVO DUARTE DOS SANTOS <gustavo.dsantos11@senacsp.edu.br>
2024-09-09 17:35:23 -0300

    Estrutura de arquivos básica e começo do cabeçalho do site

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <a href="index.php"><img src="assets/img/Image.png" alt="logo"></a> 
+    <a href="cart.php"><i class="fa-solid fa-cart-shopping">Ir para o carrinho</i></a>
+</header>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Adega2L</title>
-</head>
-<body>
-    
-    
-
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <?php include 'header.php'?>
+    <main class="container">
+        <nav class="nav-products">
+            <ul>
+                <li><a href="bebidas.php"></a>Bebidas</li>
+                <li><a href="alcoolicas.php"></a>BebidasAlcoólicas</li>
+                <li><a href="snacks.php"></a>Snacks</li>
+                <li><a href="cigarros.php"></a>Cigarros</li>
+            </ul>
+        </nav>
+        <div class="destaques">
+            <div class="destaques-bebidas">
+                <h2>PRODUTOS EM DESTAQUE SEMANAIS</h2>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="destaques-alcoolicas">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="destaques-snacks">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="destaques-Cigarros">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <script src="assets/scripts/script.js"></script>
 </body>
+<script src="https://kit.fontawesome.com/149b000a36.js" crossorigin="anonymous"></script>
 </html>
