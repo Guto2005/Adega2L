dd4c6cf159f5630b5138097deec3a81c1f156439
GUSTAVO DUARTE DOS SANTOS <gustavo.dsantos11@senacsp.edu.br>
2024-09-20 17:21:09 -0300

    Criando o modal do perfil e ajustes no site

--- header.php
+++ header.php
@@ -13,13 +13,41 @@
     <input class="search-placeholder" type="text" placeholder="Olá, O que você procura?" />
     <button class="botao-enviar" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
 </div>
-<div class="profile-link">
-    <a href="#perfil"></a>
-    <h3 class="perfil-titulo">Perfil</h3>
-    <i id="user-icon" class="fa-solid fa-user"></i>
-</div>
+<div class="perfil-link">
+<form>
+    <button id="perfil-btn" class="perfil-btn" >
+        <h3 class="perfil-titulo">Perfil</h3>
+        <i id="user-icon" class="fa-solid fa-user"></i>
+    </button>
+</form>
+</div>  
     <a class="link-carrinho" href="./cart.php"><i class="fa-solid fa-cart-shopping" id="cart-icon"><h3 class="carrinho-titulo">Minhas Compras</h3></i></a>
 </header>
+<div id="profile-modal" class="modal">
+        <div class="modal-content">
+            <span class="close-btn">&times;</span>
+            <h2>Perfil</h2>
+            <nav>
+                <a href="#informacoes">Informações</a>
+                <a href="#configuracoes">Configurações</a>
+            </nav>
+            <form>
+                <div>
+                    <label for="nome">Nome:</label>
+                    <input type="text" id="nome" name="nome" required>
+                </div>
+                <div>
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div>
+                    <label for="senha">Senha:</label>
+                    <input type="password" id="senha" name="senha" required>
+                </div>
+                <button type="submit">Salvar</button>
+            </form>
+        </div>
+    </div>
 <nav class="nav-products">
             <ul class="nav-list">
                 <li><a href="bebidas.php">Bebidas</a></li>
