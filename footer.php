f7fe612db3261392878d9656a90ea1cc3711c490
GUSTAVO DUARTE DOS SANTOS <gustavo.dsantos11@senacsp.edu.br>
2024-10-17 17:21:54 -0300

    feito as areas do footer, agora falta o design das páginas das ancoras respectivas

--- footer.php
+++ footer.php
@@ -1,20 +1,70 @@
 <footer>
         <div class="footer-container">
-            <div class="footer-left">
+            <div class="FAQ">
+                <h2>Perguntas Frequentes</h2>
+                <ul>
+                    <li>
+                        <a href="perguntas.php">pergunta1</a>
+                    </li>
+                    <li>
+                        <a href="perguntas.php">pergunta2</a>
+                    </li>
+                    <li>
+                        <a href="perguntas.php">pergunta3</a>
+                    </li>
+                    <li>
+                        <a href="perguntas.php">pergunta4</a>
+                    </li>
+                    <li>
+                        <a href="perguntas.php">pergunta5</a>
+                    </li>
+                    <li>
+                        <a href="perguntas.php">pergunta6</a>
+                    </li>
+                </ul>
+                <!-- ainda falta perguntar sobre quais serão as perguntas -->
+            </div>
+            <div class="sobre-nos">
+                <h2 class="sobre-titulo">Sobre Nós</h2>
+                <ul>
+                    <li><a href="sobrenos.php"><h3 class="sobre-subtitulo">Breve Descrição</h3></a></li>
+                    <li><a href="sobrenos.php"><h3 class="sobre-subtitulo">História da nossa Adega</h3></a></li>
+                    <li><a href="sobrenos.php"><h3 class="sobre-subtitulo">Nossos Valores</h3></a></li>
+                    <li><a href="sobrenos.php"><h3 class="sobre-subtitulo">Nossa Equipe</h3></a></li>
+                    <!-- ainda falta perguntar sobre quais outros conteúdos serão necessários -->
+                </ul>
+                
+            </div>
+            <div class="socials">
+            <div>
                 <h3>ADEGA2L</h3>
                 <p>&copy; 2024 ADEGA2L. Todos os direitos reservados.</p>
             </div>
-            <div class="footer-center">
-                <h4>Contatos</h4>
-                <p>Email: contato@adega2L.com</p>
-                <p>Telefone: (11) 1234-5678</p>
-                <p>Endereço: Rua Exemplo, 123, Cidade, Estado</p>
+                <div>
+                    <h4>Contatos</h4>
+                    <p>Email: contato@adega2L.com</p>
+                    <p>Telefone: (11) 1234-5678</p>
+                </div>
+                <div>
+                    <h4>Redes Sociais</h4>
+                    <a href="#">Facebook</a>
+                    <div class="instagram">
+                        <a href="https://www.instagram.com/adega2l" target="_blank">
+                        <i id="insta-icon" class="fab fa-instagram-square"></i>
+                        <h3>Siga nossa Adega no Instagram!</h3>
+                        </a>
+                    </div>
+
+                    <a href="#">Twitter</a>
+                </div>
             </div>
-            <div class="footer-right">
-                <h4>Redes Sociais</h4>
-                <a href="#">Facebook</a>
-                <a href="#">Instagram</a>
-                <a href="#">Twitter</a>
+            <div class="atendimento">
+                <h2>Atendimento</h2>
+                <ul>
+                    <li>Segunda a Sexta: 08:00 ÀS 00:00</li>
+                <!-- ainda falta perguntar os outros horarios  -->
+                </ul>
+                <p>Endereço: Rua José Rodrigues Martins, 507, Bairro:Nova São Vicente , São Vicente, São Paulo</p>
             </div>
         </div>
     </footer>
