4929badebb69f618e84210575bc3c68a97a3709a
GUSTAVO DUARTE DOS SANTOS <gustavo.dsantos11@senacsp.edu.br>
2025-01-22 16:45:39 -0300

    deixando organizado o footer, header e o catálogo, mudando css e java script.

--- footer.php
+++ footer.php
@@ -3,9 +3,6 @@
         <div class="footer-institucional">
         <link rel="stylesheet" href="./assets/css/footer.css" />
             <div class="footer-left">
-                <a class="logotipo-footer" href="catalogo.php"><img src="./assets/img/Image.png" alt="logo"></a>
-            </div>
-            <div class="footer-center">
                 <h4>Redes Sociais</h4>
                 <div class="redes-sociais">
                     <a href="#"><i id="facebook-icon" class="fab fa-facebook"></i></a>
@@ -14,15 +11,13 @@
                 </div>
             </div>
             <div class="footer-right">
-                <div class="contato">
+                <div class="contato-endereco">
                     <h4>Contato</h4>
-                    <p>(13) 98765-4321</p>
-                </div>
-                <div class="endereco">
+                    <p class="numero-telefone">(13) 98765-4321</p>
                     <h4>Endereço</h4>
-                    <p>R. José Rodrigues Martins, 507 - Samarita</p>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3645.1951850189216!2d-46.4863148!3d-23.9888837!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce18d2670d46ff%3A0x88dcdb11dbfebf40!2sR.%20Jos%C3%A9%20Rodrigues%20Martins%2C%20507%20-%20Samarita%2C%20S%C3%A3o%20Vicente%20-%20SP%2C%2011346-310!5e0!3m2!1spt-BR!2sbr!4v1737567319073!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <p class="endereco-loja">R. José Rodrigues Martins, 507 - Samarita</p>
                 </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3645.1951850189216!2d-46.4863148!3d-23.9888837!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce18d2670d46ff%3A0x88dcdb11dbfebf40!2sR.%20Jos%C3%A9%20Rodrigues%20Martins%2C%20507%20-%20Samarita%2C%20S%C3%A3o%20Vicente%20-%20SP%2C%2011346-310!5e0!3m2!1spt-BR!2sbr!4v1737567319073!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </footer>
